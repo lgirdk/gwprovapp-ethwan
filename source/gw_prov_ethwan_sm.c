08376e9c9d56eba98b82b44d93950e9ffd2d8bef
Rob Vogelaar <revogelaar@gmail.com>
2022-03-23 12:10:14 -0700

    use /var/tmp for CM logs
    Signed-off-by: Rob Vogelaar <revogelaar@gmail.com>

--- source/gw_prov_ethwan_sm.c
+++ source/gw_prov_ethwan_sm.c
@@ -1875,7 +1875,7 @@ static void GWP_CreateTFTPServerForCMConsoleLogs()
  	if (!tftpserverStarted)
  	{	
 	 	//snprintf(cmd,sizeof(cmd), "/usr/bin/udpsvd -E %s 69 /usr/sbin/tftpd -c /var/tftpboot &", EROUTER_PRIV_NET(40));
-          	snprintf(cmd,sizeof(cmd), "/usr/bin/udpsvd -E %s 69 /usr/sbin/tftpd -c /var/tftpboot &","172.31.255.40");
+		snprintf(cmd,sizeof(cmd), "/usr/bin/udpsvd -E %s 69 /usr/sbin/tftpd -c /var/tmp &","172.31.255.40");
 		if (system(cmd) == -1)
 			fprintf(stderr,"%s failed\n", cmd);
 		else
